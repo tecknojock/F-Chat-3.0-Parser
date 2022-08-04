891f103fcb51ec014edf868befb8e98f8467402a
tecknojock <tecknojock@gmail.com>
2022-08-04 10:07:23 -0500

    Update to 1.1

--- flistparser.py
+++ flistparser.py
@@ -8,7 +8,7 @@ import traceback
 #                                    F-Chat 3.0 Log Parser                                     #
 #                                      Author: GyroTech                                        #
 #                                                                                              #
-#                                       1.0-8/3/2022                                           #
+#                                       1.1-8/4/2022                                           #
 #                                                                                              #
 # This script parses out the database logs that fchat spits out, and then turns them into      #
 # plain text. The script automatically remembers when you last ran it, and will append to      #

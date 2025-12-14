12bc2790beedde8d46dd482c69e37cc442a2b8c6
tecknojock <tecknojock@gmail.com>
2025-12-14 15:39:34 -0600

    Update flistparser.py
    Made the parser more robust. Instead of trusting the lines, it looks for user names.

--- flistparser.py
+++ flistparser.py
@@ -8,7 +8,7 @@ import traceback
 #                                    F-Chat 3.0 Log Parser                                     #
 #                                      Author: GyroTech                                        #
 #                                                                                              #
-#                                       1.1.2- 06/12/2023                                      #
+#                                       1.1.3- 12/14/2025                                      #
 #                                                                                              #
 # This script parses out the database logs that fchat spits out, and then turns them into      #
 # plain text. The script automatically remembers when you last ran it, and will append to      #
@@ -22,15 +22,17 @@ import traceback
 
 
 #Variables for log locations
-flistDirectory = "C:\\Users\\jdavis\\Documents\\New folder\\Test" # Link to flist log directory
-logDirectory = "C:\\Users\\jdavis\\Documents\\New folder\\Test2" # Link to where you'd like the logs to go.
+flistDirectory = "D:\\Users\\Tecknojock\\Documents\\Test" # Link to flist log directory
+logDirectory = "D:\\Users\\Tecknojock\\Documents\\Test2" # Link to where you'd like the logs to go.
 logExt = ".log"
-splitBy = "month" #accepts Day, Month, Year and None
-folderStructure = 2 # 0 means you're pointing directly to the character you want logs from.
+splitBy = "Month" #accepts Day, Month, Year and None
+folderStructure = 0 # 0 means you're pointing directly to the character you want logs from.
                     # For 1 or 2 point to the root f-list logs folder.
                     # 1 means logs will be outputtted as {Your character}/{Other character}
                     # 2 means logs will be outputted as {Other Character}/{Your Character}
 
+
+namepattern = r"[a-zA-Z0-9 _-]+"
 
 
 class logLine():
@@ -57,7 +59,35 @@ class logLine():
     def encodeLine(self):
         self.encodedLine = "["+self.messageTime+"]"+ self.action+self.character+self.notAction+ " " +self.message+ "\n"
 
-
+def findLineStart (fr):
+    #Fix for F-list occasionally leaving strings of 0x00 when the logger breaks.
+    # Improved fix is now logger mistake agnostic. It will seek until it finds a valid user name.
+    try:
+        while fr.tell() < fileLength:
+            characterOffset = int.from_bytes(fr.read(1),"big") #last Date byte
+            if characterOffset > 1 or characterOffset == 0: 
+                characterOffset = int.from_bytes(fr.read(1),"big") #Action Bite
+                if characterOffset < 2: 
+                    characterOffset = int.from_bytes(fr.read(1),"big") #character Byte
+                    if characterOffset < 21 and characterOffset > 1 :
+                        character = (fr.read(characterOffset).decode("ISO-8859-1", "ignore"))
+                        if re.fullmatch(namepattern, character):
+                            fr.seek(-4-1-1-characterOffset, 1)
+                            break
+                        elif fr.tell() >= fileLength:
+                            raise(Exception("End of File"))
+                        else:
+                            fr.seek(-2-characterOffset,1)
+                    elif fr.tell() >= fileLength:
+                        raise(Exception("End of File"))
+                    else:
+                        fr.seek(-2,1)
+                elif fr.tell() >= fileLength:
+                    raise(Exception("End of File"))
+                else: 
+                    fr.seek(-1,1)
+    except:
+        return "EoF"
 
 
 Path(logDirectory + "\\lastrun.txt").touch()
@@ -107,16 +137,33 @@ for char in flistCharDirectory:
                         fName = re.sub(r"[/\\?%*:|\"<>\x7F\x00-\x1F]", "", fName).strip()
                         print(f +", " + fName)
                     if lastrun != 1:
+                        #check last line to see if logger needs to run
                         fr.seek(0,2)
-                        while True:
-                            fr.seek(-2,1)
-                            lineLength = int.from_bytes(fr.read(2),"little")+2
-                            fr.seek(-lineLength,1)
+                        fr.seek(-2,1)
+                        lineLength = int.from_bytes(fr.read(2),"little")+2
+                        fr.seek(-lineLength,1)
+                        lineTime = int.from_bytes(fr.read(4),"little")
+                        fr.seek(-4,1)
+                        if lineTime < lastrun:
+                            fr.seek(lineLength,1)
+                            break
+                        fr.seek(0,0) 
+                        #reset to begining, because forward traversal is safer due to weird logger errors.
+                        while 1:
+                            findLineStart(fr)
                             lineTime = int.from_bytes(fr.read(4),"little")
-                            fr.seek(-4,1)
-                            if lineTime < lastrun:
-                                fr.seek(lineLength,1)
+                            fr.seek(1,1)
+                            charlen = int.from_bytes(fr.read(1),"big")
+                            fr.seek(charlen,1)
+                            meslength = int.from_bytes(fr.read(2), "little")
+                            fr.seek(meslength+2,1)
+                            if lineTime > lastrun:
                                 break
+                        #backtrack when latest line is reached.
+                        fr.seek(-2,1)
+                        lineLength = int.from_bytes(fr.read(2),"little")+2
+                        fr.seek(-lineLength,1)
+                    findLineStart(fr)
                     lineTime = int.from_bytes(fr.read(4),"little")
                     while fr.tell() < fileLength:
                         fileTime = time.strftime(splitFormat,time.localtime(lineTime))
@@ -134,40 +181,25 @@ for char in flistCharDirectory:
                                 # action = 0x01 for action, 0x00 for speech
                                 # Name length = 1 byte
                                 # messagelength = 2 bytes, little endian
-                                # backwardsTraversalLength 2 bytes, little endian. Total lenght of all bytes in the line.
+                                # backwardsTraversalLength 2 bytes, little endian. Total length of all bytes in the line.
                                 currentLine = logLine()
                                 currentLine.setTime(lineTime)
-                                currentLine.isAction(fr.read(1))
+                                debugvar = int.from_bytes(fr.read(1),"big")
+                                currentLine.isAction(debugvar)
                                 characterOffset = int.from_bytes(fr.read(1),"big")
-                                if characterOffset == 0:
-                                    #Fix for F-list occasionally leaving strings of 0x00 when the logger breaks.
-                                    while characterOffset == 0:
-                                        characterOffset = int.from_bytes(fr.read(1),"big")
-                                    inline = 1
-                                    count = 1
-                                    while inline < 3:
-                                        characterOffset = int.from_bytes(fr.read(1),"big")
-                                        if characterOffset > 1:
-                                            inline += 1
-                                        else:
-                                            inline = 0
-                                        count += 1
-                                    if count == 3:
-                                        characterOffset = int.from_bytes(fr.read(1),"big")
-                                        if characterOffset < 2: # 0 or 1 means its an action next, so our first byte is a 0.
-                                            fr.seek(-5,1)
-                                        else:
-                                            fr.seek(-4,1)
-                                    else:
-                                        fr.seek(-8,1)
-                                    lineTime = int.from_bytes(fr.read(4),"little")
-                                    continue
-                                currentLine.setCharacter(fr.read(characterOffset).decode("utf-8", "ignore"))
+                                character = (fr.read(characterOffset).decode("utf-8", "ignore"))
+                                currentLine.setCharacter(character)
                                 messageOffset = int.from_bytes(fr.read(2),"little")
-                                currentLine.setMessage(fr.read(messageOffset).decode("utf-8", "ignore"))
+                                message = fr.read(messageOffset).decode("utf-8", "ignore")
+                                currentLine.setMessage(message)
                                 fr.read(2) #Last 2 bytes for reverse traversal
+                                if currentLine.character == "":
+                                    break
                                 currentLine.encodeLine()
-                                fw.writelines(currentLine.encodedLine)    
+                                fw.writelines(currentLine.encodedLine) 
+                                #Fix for F-list occasionally leaving strings of 0x00 when the logger breaks.
+                                # Improved fix is now logger mistake agnostic. It will seek until it finds a valid user name.
+                                findLineStart(fr)
                                 lineTime = int.from_bytes(fr.read(4),"little")
                                 if time.strftime(splitFormat, time.localtime(lineTime)) != fileTime:
                                     if lineTime == 0: 
@@ -180,4 +212,3 @@ for char in flistCharDirectory:
     except OSError:
                 continue
 open(logDirectory +"\\lastrun.txt","a").writelines(f"{runTime}\n")
-        
